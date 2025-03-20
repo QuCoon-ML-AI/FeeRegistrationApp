c29c704a8460c6eb0be145cc96d068b7a9f7e975
QuCoon AI, DA & ML <machinelearningteam@qucoon.com>
2025-03-20 11:42:06 +0100

    Initial commit for FeeRegistrationApp

---
+++ src/main/kotlin/com/pwb/feeRegistrationApp/FeeRegistrationAppApplication.kt
@@ -0,0 +1,13 @@
+//TODO ==== BUILT WITH QOONITY VERSION-1.0 - YINKA-ANTHONY-NKAY ON 2025-03-20T10:41:44.303666300
+package com.pwb.feeRegistrationApp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FeeRegistrationAppApplication
+
+fun main(args: Array<String>) {
+	runApplication< FeeRegistrationAppApplication>(*args)
+}
+
